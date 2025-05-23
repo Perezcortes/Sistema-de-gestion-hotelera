3785a69db77d861e9d9ae9a44374ec09321f6f83
Perezcortes <perezcortes05prz@gmail.com>
2025-05-23 02:54:37 -0600

    API y fetch para comunicacion entre backend y frontend

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -25,7 +25,7 @@ const RegisterPage: React.FC = () => {
     }
 
     try {
-      const res = await fetch("/api/auth/register", {
+      const res = await fetch("http://localhost:3000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ nombre, email, username, password }),
